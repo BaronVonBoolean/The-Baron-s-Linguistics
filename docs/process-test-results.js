297bb6dc32953067ee1c74436e821fb6f2236eb3
Ian Culleton <ianculleton@Ians-MacBook-Pro.local>
2025-03-20 17:48:14 -0400

    continues building test report page

--- docs/process-test-results.js
+++ docs/process-test-results.js
@@ -1,20 +1,27 @@
 const results  = fetch('./test-results.json')
-  .then(response => response.json())
-  .then(parsedResults => {
-    const summary = document.getElementById('test-results-summary');
-    const testResultsElement = document.getElementById('test-results');
+.then(response => response.json())
+.then(parsedResults => {
+  const summary = document.getElementById('test-results-summary');
+  const testResultsElement = document.getElementById('test-results');
 
-    parsedResults.testResults.forEach(file => {
-      console.log('>>>', file);
-      const fileBlock = document.createElement('div');
-      fileBlock.innerHTML = `<div>${ file.name.split('/').pop() }</div>`;
-      testResultsElement.append(fileBlock);
-    })
+  parsedResults.testResults.forEach(file => makeTestResultBlock(file));
 
-    summary.innerHTML = `
-      <span class="passed">${parsedResults.numPassedTests}</span> passed, 
-      <span class="failed">${parsedResults.numFailedTests}</span> failed, 
-      <span class="pending">${parsedResults.numPendingTests}</span> pending
-    `;
-  });
+  summary.innerHTML = `
+    <span class="passed">${parsedResults.numPassedTests}</span> passed, 
+    <span class="failed">${parsedResults.numFailedTests}</span> failed, 
+    <span class="pending">${parsedResults.numPendingTests}</span> pending
+  `;
+});
 
+function makeDescribeBlock(describe) {
+  const describeBlock = document.createElement('div');
+  describeBlock.innerHTML = `<div>${ describe.title }</div>`;
+  return describeBlock;
+}
+
+function makeTestResultBlock(testResult) {
+  const testResultFileHeader = document.createElement('div');
+  testResultFileHeader.innerHTML = `<div>${ testResult.name.split('/').pop().split('.').shift() }</div>`;
+  testResult.assertionResults.forEach(assertion => makeDescribeBlock(assertion));
+  return testResultFileHeader;
+}
