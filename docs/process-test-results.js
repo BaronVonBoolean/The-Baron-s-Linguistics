16c0bd6ecebc38270fb9a33dc3d327e290a6cd86
Ian Culleton <ianculleton@Ians-MacBook-Pro.local>
2025-03-20 17:32:20 -0400

    adds js file to ghpages

--- docs/process-test-results.js
+++ docs/process-test-results.js
@@ -1,6 +1,9 @@
 const results  = fetch('./test-results.json')
   .then(response => response.json())
   .then(parsedResults => {
+    const summary = document.getElementById('test-results-summary');
+    console.log(summary);
+
     summary.innerHTML = `
       <span>${parsedResults.numPassedTests}</span> tests passed, 
       <span>${parsedResults.numFailedTests}</span> tests failed, 
@@ -8,5 +11,3 @@ const results  = fetch('./test-results.json')
     `;
   });
 
-  const summary = document.getElementById('test-results-summary');
-console.log(summary);
