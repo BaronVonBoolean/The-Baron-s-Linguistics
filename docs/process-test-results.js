03fb3ec5a1cff31cd4bc42c5fbbbf288e76f6a16
Ian Culleton <ianculleton@Ians-MacBook-Pro.local>
2025-03-20 17:40:18 -0400

    more building out of test reporter

--- docs/process-test-results.js
+++ docs/process-test-results.js
@@ -2,12 +2,18 @@ const results  = fetch('./test-results.json')
   .then(response => response.json())
   .then(parsedResults => {
     const summary = document.getElementById('test-results-summary');
-    console.log(summary);
+    const testResultsElement = document.getElementById('test-results');
+
+    parsedResults.testResults.forEach(file => {
+      const fileBlock = document.createElement('div');
+      fileBlock.innerHTML = `<h3>${ file.name.split('/').pop() }</h3>`;
+      testResultsElement.append(fileBlock);
+    })
 
     summary.innerHTML = `
-      <span>${parsedResults.numPassedTests}</span> tests passed, 
-      <span>${parsedResults.numFailedTests}</span> tests failed, 
-      <span>${parsedResults.numPendingTests}</span> tests pending
+      <span class="passed">${parsedResults.numPassedTests}</span> passed, 
+      <span class="failed">${parsedResults.numFailedTests}</span> failed, 
+      <span class="pending">${parsedResults.numPendingTests}</span> pending
     `;
   });
 
